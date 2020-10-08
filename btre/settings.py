cb286ce8d36ae51d0fb73c3ae534b30828edf118
BON98 <bornfaceotieno35@gmail.com>
2020-10-08 23:40:49 +0300

    update

--- btre/settings.py
+++ btre/settings.py
@@ -139,12 +139,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-#email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bornfaceotieno35@gmail.com'
-EMAIL_HOST_PASSWORD= ''
-EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
